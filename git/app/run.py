d77fe1b23a0cb6fc0683ce811bb18bd726a02c66
Antonykirunyu <kirunyuanthony@gmail.com>
2020-04-22 17:47:49 +0300

    initial commit

--- git/app/run.py
+++ git/app/run.py
@@ -1,8 +1,4 @@
-from flask import Flask, escape, request
+from app import app
 
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+if __name__ == '__main__':
+    app.run(debug = True)
