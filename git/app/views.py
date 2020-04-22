983833d0f025666e8eeaab41e14553983904880b
Antonykirunyu <kirunyuanthony@gmail.com>
2020-04-22 18:33:39 +0300

    added files

--- git/app/views.py
+++ git/app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-
+from .request import get_movies
 # Views
 @app.route('/')
 def index():
@@ -8,8 +8,13 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+    # Getting popular movie
+    popular_movies = get_movies('popular')
+    print(popular_movies)
+    upcoming_movie = get_movies('upcoming')
+    now_showing_movie = get_movies('now_playing')
     title = 'Home - Welcome to The best Movie Review Website Online'
-    return render_template('index.html', title = title)
+    return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie)
 
 @app.route('/movie/<int:movie_id>')
 def movie(movie_id):
