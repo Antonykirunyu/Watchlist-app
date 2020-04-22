867d335bd21ef36847564c0248fa93987f47b14c
Antonykirunyu <kirunyuanthony@gmail.com>
2020-04-22 19:25:10 +0300

    added bootstrap

--- git/app/__init__.py
+++ git/app/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask, escape, request, views
 from .config import DevConfig
+from flask_bootstrap import Bootstrap
+from app import error
+from app import views
 app = Flask(__name__,instance_relative_config=True)
 
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
+
+bootstrap = Bootstrap(app)
 
 from app import views
 
--- git/app/views.py
+++ git/app/views.py
@@ -1,8 +1,8 @@
 from flask import render_template
 from app import app
-from .request import get_movies
+from .requests import get_movies,get_movie
 # Views
-@app.route('/')
+@app.route('/movie/<int:id>')
 def index():
 
     '''
